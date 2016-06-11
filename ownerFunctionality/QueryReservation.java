edcdc4a18854c50d8c0cd94f39f992b5ca5180d7
UnitaryOperator <jimmydu4444@gmail.com>
2016-06-11 12:04:19 -0700

    query reservation stub

---
+++ ownerFunctionality/QueryReservation.java
@@ -0,0 +1,20 @@
+package ownerFunctionality;
+
+import java.sql.*;
+import java.awt.*;
+import javax.swing.*;
+
+import utility.*;
+
+public class QueryReservation {
+
+	private Timestamp beginTime = null;
+	private Connection connection;
+	
+	private String businessName;
+	private int businessID;
+	
+	
+	
+	
+}
